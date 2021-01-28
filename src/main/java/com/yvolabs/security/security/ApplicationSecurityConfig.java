a57be730961149412060ce45a6cb5948fc1228c8
yvonnkr <34454562+yvonnkr@users.noreply.github.com>
2021-01-28 23:37:23 +0000

    How to enable csrf and Generate token

--- src/main/java/com/yvolabs/security/security/ApplicationSecurityConfig.java
+++ src/main/java/com/yvolabs/security/security/ApplicationSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import static com.yvolabs.security.security.ApplicationUserRole.*;
 
@@ -30,7 +31,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable() // TODO: More on csrf later
+                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
