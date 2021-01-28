4f01a44e42a65a8aa419094cf69522a1e8c7571b
yvonnkr <34454562+yvonnkr@users.noreply.github.com>
2021-01-28 00:41:16 +0000

    Added new user InMemoryUserDetailsManager

--- src/main/java/com/yvolabs/security/security/ApplicationSecurityConfig.java
+++ src/main/java/com/yvolabs/security/security/ApplicationSecurityConfig.java
@@ -1,16 +1,20 @@
 package com.yvolabs.security.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // super.configure(http);
         /** Basic Auth example
          * -> will perform a basic auth from a web browser a popup window(modal) -> username/password
          * -> Or if using eg. postman /react app -> set request headers -> Authorization    Basic ***the user&password as base64****
@@ -18,11 +22,23 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
          */
         http
                 .authorizeRequests()
-                .antMatchers("/","index","/css/*","/js/*")
+                .antMatchers("/", "index", "/css/*", "/js/*")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails johnDoeUser = User.builder()
+                .username("johndoe")
+                .password("$$123456")
+                .roles("STUDENT") //ROLE_STUDENT
+                .build();
+
+        return new InMemoryUserDetailsManager(johnDoeUser);
     }
 }
