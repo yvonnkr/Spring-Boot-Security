1ea1e157444682f977ed0264f8c50e6aa5603ab8
yvonnkr <34454562+yvonnkr@users.noreply.github.com>
2021-01-28 19:46:16 +0000

    Permision based authentication on method level using Annotations - @PreAuthorise

--- src/main/java/com/yvolabs/security/security/ApplicationSecurityConfig.java
+++ src/main/java/com/yvolabs/security/security/ApplicationSecurityConfig.java
@@ -3,7 +3,7 @@ package com.yvolabs.security.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,11 +13,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-import static com.yvolabs.security.security.ApplicationUserPermission.*;
 import static com.yvolabs.security.security.ApplicationUserRole.*;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     private final PasswordEncoder passwordEncoder;
 
@@ -39,10 +39,12 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
-                .antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-                .antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-                .antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-                .antMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ADMIN.name(), ADMIN_TRAINEE.name())
+                /** 1. NOTE: Order does matters!! with antMatchers()
+                 *  2. used preAuthorized annotations instead */
+//                .antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ADMIN.name(), ADMIN_TRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
--- src/main/java/com/yvolabs/security/student/StudentManagementController.java
+++ src/main/java/com/yvolabs/security/student/StudentManagementController.java
@@ -1,5 +1,6 @@
 package com.yvolabs.security.student;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -15,13 +16,17 @@ public class StudentManagementController {
 
     );
 
+    /** PreAuthorize args: hasRole('ROLE_'), hasAnyRole('ROLE_'), hasAuthority('permission'), hasAnyAuthority('permission')  */
+
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_ADMIN_TRAINEE')")
     public List<Student> getAllStudents() {
 
         return STUDENTS;
     }
 
     @GetMapping("{studentId}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_ADMIN_TRAINEE')")
     public Student getStudent(@PathVariable("studentId") Integer studentId) {
 
         return STUDENTS.stream()
@@ -32,16 +37,19 @@ public class StudentManagementController {
 
 
     @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
     public void registerNewStudent(@RequestBody Student student) {
         System.out.println(String.format("ADDED: %s", student.toString()));
     }
 
     @DeleteMapping("{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void deleteStudent(@PathVariable("studentId") Integer studentId) {
         System.out.println(String.format("DELETE: student with id %s deleted", studentId));
     }
 
     @PutMapping("{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void updateStudent(@PathVariable("studentId") Integer studentId, @RequestBody Student student) {
 
         System.out.println(String.format("UPDATED: ID:%s %s", studentId, student.toString()));
