62dd4b08f5d247852a0f94786f9adb5e78e12697
yvonnkr <34454562+yvonnkr@users.noreply.github.com>
2021-01-29 19:24:54 +0000

    Authentication Request and Attempt Authentication

---
+++ src/main/java/com/yvolabs/security/jwt/UsernameAndPasswordAuthenticationRequest.java
@@ -0,0 +1,25 @@
+package com.yvolabs.security.jwt;
+
+public class UsernameAndPasswordAuthenticationRequest {
+    private String userName;
+    private String password;
+
+    public UsernameAndPasswordAuthenticationRequest() {
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
